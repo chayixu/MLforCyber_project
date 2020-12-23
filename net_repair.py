4f72c8e5115f5f65d7fb3a8853bb29b4881d456f
nananke <44246023+nananke@users.noreply.github.com>
2020-12-24 01:27:48 +0800

    make change

--- net_repair.py
+++ net_repair.py
@@ -30,24 +30,71 @@ def main():
 
     bd_model = keras.models.load_model(model_filename) 
     clean_label_p = np.argmax(bd_model.predict(x_test), axis=1)
-
+    print(bd_model.summary())
     #find the last ReLU activation layer
-    layer_name = 'activation_1'
+    layer_name = 'conv_4'
     #get the output value for each neuron
     intermediate_layer_model = Model(inputs=bd_model.input,
                                      outputs=bd_model.get_layer(layer_name).output)
     #shows the intermediate_output for layer activation layer
     intermediate_output = intermediate_layer_model.predict(x_test)
+    print(intermediate_output)
     nueron_score = []
     for x in range(0,len(intermediate_output)):
         #average score
         avg = mean(intermediate_output[x])
-        nueron_score.append(avg)
-    nueron_score.sort()  
+        tot = []
+        tot.append(x)
+        tot.append(avg)
+        nueron_score.append(tot)
+    sorted_value  = sorted(nueron_score, key=lambda x: x[1], reverse=True)
+    #for x in range (0,len(nueron_score)/2):
+    #    print(sorted_value[x])
+    #create a mask layer that has the same number of number of nuerons in previous activatoin function 
+    mask_layer = [1] * len(nueron_score)
+    #print(mask_layer)
     class_accu = np.mean(np.equal(clean_label_p, y_test))*100
-    print('Classification accuracy:', class_accu)
-    new_model = bd_model
+    repaired_model = create_repair_model(mask_layer)
+    print('Old Classification accuracy:', class_accu)
 
+    new_label = np.argmax(repaired_model.predict(x_test), axis=1)
+    class_accu = np.mean(np.equal(new_label, y_test))*100
+    print('repaired Classification accuracy:', class_accu)
+
+def create_repair_model(mask):
+	# define input
+	x = keras.Input(shape=(55, 47, 3), name='input')
+	# feature extraction
+	conv_1 = keras.layers.Conv2D(20, (4, 4), activation='relu', name='conv_1')(x)
+	pool_1 = keras.layers.MaxPooling2D((2, 2), name='pool_1')(conv_1)
+	conv_2 = keras.layers.Conv2D(40, (3, 3), activation='relu', name='conv_2')(pool_1)
+	pool_2 = keras.layers.MaxPooling2D((2, 2), name='pool_2')(conv_2)
+	conv_3 = keras.layers.Conv2D(60, (3, 3), activation='relu', name='conv_3')(pool_2)
+	pool_3 = keras.layers.MaxPooling2D((2, 2), name='pool_3')(conv_3)
+	# first interpretation model
+	flat_1 = keras.layers.Flatten()(pool_3)	
+	fc_1 = keras.layers.Dense(160, name='fc_1')(flat_1)
+	# second interpretation model
+    #x1 =  Lambda(lambda x: x * mask)
+    #y = tf.keras.layers.Add()([fc_1, x2])
+	conv_4 = keras.layers.Conv2D(80, (2, 2), activation='relu', name='conv_4')(pool_3)
+	flat_2 = keras.layers.Flatten()(conv_4)
+	fc_2 = keras.layers.Dense(160, name='fc_2')(flat_2)
+	# merge interpretation
+	merge = keras.layers.Add()([fc_1, fc_2])
+	add_1 = keras.layers.Activation('relu')(merge)
   
+	drop = keras.layers.Dropout(0.8)
+	# output
+	y_hat = keras.layers.Dense(1283, activation='softmax', name='output')(add_1)
+	model = keras.Model(inputs=x, outputs=y_hat)
+	# summarize layers
+	#print(model.summary())
+	# plot graph
+	#plot_model(model, to_file='model_architecture.png')
+
+	return model
+
+
 if __name__ == '__main__':
     main()
